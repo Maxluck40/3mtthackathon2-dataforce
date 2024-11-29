9c719f946546f8e6e55dd68cba888d96a096315c
Maxluck40 <dennismaxwell26@gmail.com>
2024-11-29 14:23:25 +0100

    Delete run_streamlit_app.bat

--- run_streamlit_app.bat
+++
@@ -1,8 +0,0 @@
-conda activate myenv
-cd "C:\Users\SURFACE\Downloads\workspace (3)\workspace"
-streamlit run app.py
-
-
-conda install -n myenv streamlit
-conda install streamlit
-conda install scikit-learn
